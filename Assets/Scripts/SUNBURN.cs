dc63f1ee6fb8198e82e42fbe079b5cfa730e1edb
7Sunburns <132106786+7Sunburns@users.noreply.github.com>
2023-08-27 15:42:59 -0700

    Sub
    nurns

---
+++ Assets/Scripts/SUNBURN.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SUNBURN : MonoBehaviour
+{
+    
+    void Update()
+    {
+        Debug.Log("SUNBURNS WAS  HERE");
+    }
+}
